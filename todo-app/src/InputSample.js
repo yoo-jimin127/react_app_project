abeb6b054445be30e5cd48d258bf68b9d675fc60
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-06 11:25:17 +0900

    [1006] useState를 사용한 input 값의 상태 관리

--- todo-app/src/InputSample.js
+++ todo-app/src/InputSample.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function InputSample() {
+  const [text, setText] = useState('');
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onReset = () => {
+    setText('');
+  };
+
   return (
     <div>
-      <input />
-      <button>초기화</button>
+      <input onChange={onChange} value={text}  />
+      <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: </b>
+        <b>값: {text}</b>
       </div>
     </div>
   );
