78e06a6706e622264ab65e1d22496f0e69aa701d
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-06 22:34:55 +0900

    [1006] UserList.js 컴포넌트 추가

---
+++ todo-app/src/UserList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function UserList() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+  return (
+    <div>
+      <div>
+        <b>{users[0].username}</b> <span>({users[0].email})</span>
+      </div>
+      <div>
+        <b>{users[1].username}</b> <span>({users[1].email})</span>
+      </div>
+      <div>
+        <b>{users[2].username}</b> <span>({users[1].email})</span>
+      </div>
+    </div>
+  );
+}
+
+export default UserList;
