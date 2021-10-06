0a548bbf81fb7f85b57683a466cc8eba8848034c
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-06 11:23:59 +0900

    [1006] Input 상태 관리 + App에서의 렌더링

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -14,16 +14,15 @@
 // export default App;
 
 import React from 'react';
-import Counter from './Counter';
+import InputSample from './InputSample';
 
 function App() {
   return (
-    <Counter />
+    <InputSample />
   );
 }
 
 export default App;
-
 
 // function App() {
 //   return (
