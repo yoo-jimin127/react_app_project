9fea22440d8eea20cc1669408409b5071df5ef1c
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-06 22:35:56 +0900

    [1006] UserList 컴포넌트의 재사용성 추가

--- todo-app/src/UserList.js
+++ todo-app/src/UserList.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+function User({ user }) {
+  return (
+    <div>
+      <b>{user.username}</b> <span>({user.email})</span>
+    </div>
+  );
+}
+
 function UserList() {
   const users = [
     {
       id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com'
+      username: 'jimin',
+      email: 'public.jimin@gmail.com'
     },
     {
       id: 2,
@@ -18,17 +26,12 @@ function UserList() {
       email: 'liz@example.com'
     }
   ];
+
   return (
     <div>
-      <div>
-        <b>{users[0].username}</b> <span>({users[0].email})</span>
-      </div>
-      <div>
-        <b>{users[1].username}</b> <span>({users[1].email})</span>
-      </div>
-      <div>
-        <b>{users[2].username}</b> <span>({users[1].email})</span>
-      </div>
+      <User user={users[0]} />
+      <User user={users[1]} />
+      <User user={users[2]} />
     </div>
   );
 }
