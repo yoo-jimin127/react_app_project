2e6ff912bf77b17808e79f4470703f80222cbf0e
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-06 11:19:19 +0900

    [1006] 버튼 클릭에 따른 콘솔 출력

--- todo-app/src/Counter.js
+++ todo-app/src/Counter.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 function Counter() {
+  const onIncrease = () => {
+    console.log('+1')
+  }
+  const onDecrease = () => {
+    console.log('-1');
+  }
   return (
     <div>
       <h1>0</h1>
-      <button>+1</button>
-      <button>-1</button>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
     </div>
   );
 }
