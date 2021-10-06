df3356eeca99b9d3e8846210225e4623b4d31125
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-06 11:27:39 +0900

    [1006] 특정 DOM 선택 (By. useRef)

--- todo-app/src/InputSample.js
+++ todo-app/src/InputSample.js
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
   const [inputs, setInputs] = useState({
     name: '',
     nickname: ''
   });
+  const nameInput = useRef();
 
   const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
 
-  const onChange = (e) => {
+  const onChange = e => {
     const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
     setInputs({
       ...inputs, // 기존의 input 객체를 복사한 뒤
@@ -19,15 +20,26 @@ function InputSample() {
   const onReset = () => {
     setInputs({
       name: '',
-      nickname: '',
-    })
+      nickname: ''
+    });
+    nameInput.current.focus();
   };
-
 
   return (
     <div>
-      <input name="name" placeholder="이름" onChange={onChange} value={name} />
-      <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
+      <input
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
       <button onClick={onReset}>초기화</button>
       <div>
         <b>값: </b>
