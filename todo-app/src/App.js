3cc7a9bb1fbb6ca43f2199286aca3c9a991a4113
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-06 11:17:20 +0900

    [1006] 카운트 컴포넌트 생성 및 App.js 수정

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,17 +1,29 @@
+// import React from 'react';
+// import Hello from './Hello';
+// import Wrapper from './Wrapper';
+
+// function App() {
+//   return (
+//     <Wrapper>
+//       <Hello name="App User" color="blue" isSpecial />
+//       <Hello color="pink"/>
+//     </Wrapper>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
   return (
-    <Wrapper>
-      <Hello name="App User" color="blue" isSpecial />
-      <Hello color="pink"/>
-    </Wrapper>
+    <Counter />
   );
 }
 
 export default App;
+
 
 // function App() {
 //   return (
