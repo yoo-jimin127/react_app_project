a04242eed8786e66d541dd6da90929e8ab2bd0b7
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-06 22:40:24 +0900

    [1006] 배열 렌더링 (key 설정)

--- todo-app/src/UserList.js
+++ todo-app/src/UserList.js
@@ -29,9 +29,9 @@ function UserList() {
 
   return (
     <div>
-      <User user={users[0]} />
-      <User user={users[1]} />
-      <User user={users[2]} />
+      {users.map(user => (
+        <User user={user} key={user.id} />
+      ))}
     </div>
   );
 }
