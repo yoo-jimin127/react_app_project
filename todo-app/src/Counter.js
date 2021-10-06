a2b6c994c58312dbaf34974a47323e6cf599154c
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-06 11:21:09 +0900

    [1006] useState 추가를 통한 동적 값 설정

--- todo-app/src/Counter.js
+++ todo-app/src/Counter.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Counter() {
+  const [number, setNumber] = useState(0);
+
   const onIncrease = () => {
-    console.log('+1')
+    setNumber(number + 1);
   }
+
   const onDecrease = () => {
-    console.log('-1');
+    setNumber(number - 1);
   }
+
   return (
     <div>
-      <h1>0</h1>
+      <h1>{number}</h1>
       <button onClick={onIncrease}>+1</button>
       <button onClick={onDecrease}>-1</button>
     </div>
