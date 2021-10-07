72bfbcd473dea3aa6181f88ebddc6a7539855756
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-07 21:00:54 +0900

    [1007]UseRef을 사용해 컴포넌트 안에 변수 생성

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,3 +1,37 @@
+import React, { useRef } from 'react';
+import UserList from './UserList';
+
+function App() {
+  const users = [
+    {
+      id: 1,
+      username: 'jimin',
+      email: 'public.jimin@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    // ...
+
+    nextId.current += 1;
+  };
+  return <UserList users={users} />;
+}
+
+export default App;
+
 // import React from 'react';
 // import Hello from './Hello';
 // import Wrapper from './Wrapper';
@@ -13,16 +47,7 @@
 
 // export default App;
 
-import React from 'react';
-import InputSample from './InputSample';
 
-function App() {
-  return (
-    <InputSample />
-  );
-}
-
-export default App;
 
 // function App() {
 //   return (
