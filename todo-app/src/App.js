26e944cb37f5e0442b9c824b628694e06e0b5357
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-07 21:16:51 +0900

    [1007] OnRemove 구현 + UserList 전달

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -40,13 +40,19 @@ function App() {
       username,
       email
     };
-    setUsers([...users, user]);
+    setUsers(users.concat(user));
 
     setInputs({
       username: '',
       email: ''
     });
     nextId.current += 1;
+  };
+
+  const onRemove = id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter(user => user.id !== id));
   };
   return (
     <>
@@ -56,7 +62,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} />
+      <UserList users={users} onRemove={onRemove} />
     </>
   );
 }
