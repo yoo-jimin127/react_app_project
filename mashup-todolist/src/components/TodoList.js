7b283161b00a47ab10ea500a984d2d64795d078b
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-13 12:11:22 +0900

    #9 TodoList: state 조회, 렌더링 구현

--- mashup-todolist/src/components/TodoList.js
+++ mashup-todolist/src/components/TodoList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import TodoItem from './TodoItem';
+import { useTodoState } from '../TodoContext';
 
 const TodoListBlock = styled.div`
   flex: 1;
@@ -10,12 +11,18 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
+  const todos = useTodoState();
+
   return (
     <TodoListBlock>
-      <TodoItem text="운영체제 강의듣기" done={true} />
-      <TodoItem text="프로토타입 디자인하기" done={true} />
-      <TodoItem text="인공지능 강의듣기" done={false} />
-      <TodoItem text="앱프로그래밍 및 실습 과제하기" done={false} />
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          text={todo.text}
+          done={todo.done}
+        />
+      ))}
     </TodoListBlock>
   );
 }
