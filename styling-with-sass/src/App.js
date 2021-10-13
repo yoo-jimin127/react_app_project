c6a5f7a815c7ab9a36b3c0f10a8e661038ec98ea
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-13 10:55:41 +0900

    #6 props로 받은 props 값이 태그의 className으로 전달되도록 버튼 수정

--- styling-with-sass/src/App.js
+++ styling-with-sass/src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <div className="buttons">
+        <Button size="large">BUTTON</Button>
         <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
       </div>
     </div>
   );
--- styling-with-sass/src/components/Button.js
+++ styling-with-sass/src/components/Button.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import classNames from 'classnames';
 import './Button.scss';
 
-function Button({ children }) {
-  return <button className="Button">{children}</button>;
+function Button({ children, size }) {
+  return <button className={classNames('Button', size)}>{children}</button>;
 }
+
+Button.defaultProps = {
+  size: 'medium'
+};
 
 export default Button;
